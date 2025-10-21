1f9fe4c7cbf9b656ad8425c06cb120dc0cf61aeb
Supreethsj83 <supreeth@eisberg.in>
2025-10-21 18:41:10 +0530

    Rename RegisterHotKey to NativeRegisterHotKey

--- HotkeyService.cs
+++ HotkeyService.cs
@@ -9,19 +9,16 @@ namespace PostItExplorer.Services
         public const int MOD_CONTROL = 0x0002;
         public const int HOTKEY_ID = 1;
 
-        // Native P/Invoke (renamed to avoid clash)
         [DllImport("user32.dll", EntryPoint = "RegisterHotKey", SetLastError = true)]
-        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, uint vk);
+        private static extern bool NativeRegisterHotKey(IntPtr hWnd, int id, int fsModifiers, uint vk);
 
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
-        // Managed wrapper
         public static bool Register(IntPtr handle, int id, int modifiers, uint key)
         {
-            // Ignore failure if it wasn't registered before
             UnregisterHotKey(handle, id);
-            return RegisterHotKey(handle, id, modifiers, key);
+            return NativeRegisterHotKey(handle, id, modifiers, key);
         }
     }
 }
