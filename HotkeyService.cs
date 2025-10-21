186623b28791872c4a0c698057f7598d4ea7b291
Supreethsj83 <supreeth@eisberg.in>
2025-10-21 18:28:16 +0530

    Update HotkeyService.cs

--- HotkeyService.cs
+++ HotkeyService.cs
@@ -9,19 +9,19 @@ namespace PostItExplorer.Services
         public const int MOD_CONTROL = 0x0002;
         public const int HOTKEY_ID = 1;
 
-        // Rename the native import to avoid name clash
+        // Native P/Invoke (renamed to avoid clash)
         [DllImport("user32.dll", EntryPoint = "RegisterHotKey", SetLastError = true)]
-        private static extern bool HotkeyService.Register(IntPtr hWnd, int id, int fsModifiers, uint vk);
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, uint vk);
 
         [DllImport("user32.dll", SetLastError = true)]
         public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
 
-        // Managed wrapper that first unregisters, then registers
+        // Managed wrapper
         public static bool Register(IntPtr handle, int id, int modifiers, uint key)
         {
-            // ignore failure of Unregister if not previously registered
+            // Ignore failure if it wasn't registered before
             UnregisterHotKey(handle, id);
-            return HotkeyService.Register(handle, id, modifiers, key);
+            return RegisterHotKey(handle, id, modifiers, key);
         }
     }
 }
